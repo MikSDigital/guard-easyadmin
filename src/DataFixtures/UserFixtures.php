3dbbd8442ed0bcc48e0545222895061e571fe5a4
MikSDigital <mixa.ru.ru@gmail.com>
2018-04-13 21:23:10 +0300

    Fixtures

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class UserFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        for ($i = 0; $i < 5; $i++) {
+
+            $email = 'user+' . $i . '@gmail.com';
+            $user = new User();
+            $user->setEmail($email);
+
+            $manager->persist($user);
+        }
+
+        $manager->flush();
+    }
+}
